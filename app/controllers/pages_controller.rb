5eecaef1df90290b08c8e33daa5ddbc869195506
ewi-orion <brpmdr@mailfence.com>
2022-08-13 19:17:50 -0400

    add user and data

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,11 @@
 class PagesController < ApplicationController
+  
   def home
+    @people = Person.all
   end
 
   def download
+
     pdf = Prawn::Document.new
     pdf.text 'texto del pdf'
 
@@ -13,6 +16,17 @@ class PagesController < ApplicationController
   end
 
   def preview
+    pdf = Prawn::Document.new
+    pdf.text 'texto del pdf preview'
+    pdf.text 'this is the preview text'
+    pdf.start_new_page
+    pdf.text 'this is a new page...'
+
+    send_data(pdf.render,
+      filename: 'texto.pdf',
+      type: 'application/pdf',
+      disposition: 'inline'
+    )
   end
   
 end
